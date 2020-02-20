6e0db40b2bbb5cb36bb943e19fbad447ae7dd3d7
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-02-20 13:49:54 -0500

    Name fades away

---
+++ assets/portfolio.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $(document).scroll(function () {
+        scroll_pos = $(this).scrollTop();
+        percent = scroll_pos/(466.25);
+        opacityPercent = 1 - percent;
+        realOpacityPercent = opacityPercent*100;
+        if (scroll_pos > -1 && scroll_pos < 466.25) {
+            $("#display").css('margin-top', scroll_pos+'px');
+            $("#display").css('opacity', realOpacityPercent + '%');
+        } else 
+        {
+            $("#display").css('opacity', '0%');
+        }
+
+
+      
+
+        console.log("opacityPercent"+opacityPercent)
+    });
+});
