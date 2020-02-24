9e1011f7d5c6653ab5cfc66918df8b1e3daa071f
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-02-24 13:24:14 -0500

    changed xl bootstrap classes

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -2,37 +2,36 @@ $(document).ready(function () {
     var scroll_pos = 0;
     $(document).scroll(function () {
         scroll_pos = $(this).scrollTop();
-        percent = scroll_pos/(456.25);
+        percent = scroll_pos / (456.25);
         opacityPercent = 1 - percent;
-        realOpacityPercent = opacityPercent*100;
-        var screenWidth =window.innerWidth;
+        realOpacityPercent = opacityPercent * 100;
+        var screenWidth = window.innerWidth;
         if (scroll_pos > -1 && scroll_pos < 456.25) {
-            $("#display").css('margin-top', scroll_pos+'px');
+            $("#display").css('margin-top', scroll_pos + 'px');
             $("#display").css('opacity', realOpacityPercent + '%');
             $(".nav-link").css('color', 'white');
             $(".nav-link").css('background', 'rgba(0, 0, 0, 0)');
         }
-        if (scroll_pos > 456.25 && scroll_pos< 586.25)
-        {
+        if (scroll_pos > 456.25 && scroll_pos < 586.25) {
             $("#display").css('opacity', '0%');
             $(".nav-link").css('color', 'white');
             $(".nav-link").css('background', 'rgba(0, 0, 0, 0)');
-        } 
+        }
         if (scroll_pos > 586.25) {
 
-           
+
             $(".nav-link").css('background', 'rgba(0, 0, 0, 1)');
             $(".nav-link").css('color', 'gray');
 
         }
-      
 
 
 
 
 
-        console.log("scrollPos"+scroll_pos);
-        console.log("screenWidth"+screenWidth);
+
+        console.log("scrollPos" + scroll_pos);
+        console.log("screenWidth" + screenWidth);
     });
 });
 
