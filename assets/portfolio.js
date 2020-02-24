4ee0f72b1da228c5825e84c515b389ce76ef0409
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-02-23 20:51:12 -0500

    scrollbar tells what ssection

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         percent = scroll_pos/(456.25);
         opacityPercent = 1 - percent;
         realOpacityPercent = opacityPercent*100;
+        var screenWidth =window.innerWidth;
         if (scroll_pos > -1 && scroll_pos < 456.25) {
             $("#display").css('margin-top', scroll_pos+'px');
             $("#display").css('opacity', realOpacityPercent + '%');
@@ -24,8 +25,14 @@ $(document).ready(function () {
             $(".nav-link").css('color', 'gray');
 
         }
+      
+
+
+
+
 
         console.log("scrollPos"+scroll_pos);
+        console.log("screenWidth"+screenWidth);
     });
 });
 
@@ -39,6 +46,43 @@ $('a').on('click', function (e) {
 
         $('html, body').animate({ scrollTop: $(hash).offset().top }, 1000);
     }
+});
+
+$('a').click(function () {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+});
+
+// Cache selectors
+var topMenu = $(".nav"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+// Bind to scroll
+$(window).scroll(function () {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function () {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+    // Set/remove active class
+    menuItems
+        .parent().removeClass("active")
+        .end().filter("[href='#" + id + "']").parent().addClass("active");
 });
 
 
